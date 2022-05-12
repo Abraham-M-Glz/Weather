f3752bb29593519c4c51b33ceabed1ab13cf9cb5
AbrahamFlutter <abraham.magana@itksquare.edu.mx>
2022-05-12 04:13:13 -0500

    wind & humidity

--- lib/home.dart
+++ lib/home.dart
@@ -27,8 +27,8 @@ class _HomeState extends State<Home> {
           decoration: new BoxDecoration(
             gradient: LinearGradient(
                 colors: [
-                  Color.fromARGB(195, 0, 68, 255),
-                  Color.fromARGB(141, 33, 91, 250)
+                  Color.fromARGB(141, 0, 68, 255),
+                  Color.fromARGB(108, 33, 91, 250)
                 ],
                 stops: [
                   0.3,
@@ -46,44 +46,71 @@ class _HomeState extends State<Home> {
                   style: TextStyle(
                       fontSize: 30,
                       fontWeight: FontWeight.bold,
-                      color: Color.fromARGB(172, 255, 255, 255)),
+                      color: Color.fromARGB(226, 255, 255, 255)),
                 ),
               ),
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Obx(
-                      () => Text(
-                        "${_controller.celsius.round()}°C",
-                        style: TextStyle(
-                            fontSize: 60,
-                            fontWeight: FontWeight.bold,
-                            color: Color.fromARGB(255, 255, 255, 255)),
-                      ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Obx(
+                    () => Text(
+                      "${_controller.celsius.round()}°C",
+                      style: TextStyle(
+                          fontSize: 60,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 255, 255, 255)),
                     ),
-                    Obx(
-                      () => Image.network(
-                        "https://openweathermap.org/img/w/${_controller.data["weather"][0]["icon"]}.png",
-                        width: 150,
-                        height: 150,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                child: Obx(
-                  () => Text(
-                    "${_controller.data["weather"][0]["description"]}",
-                    style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                        color: Color.fromARGB(172, 255, 255, 255)),
                   ),
+                  Obx(
+                    () => Image.network(
+                      "https://openweathermap.org/img/w/${_controller.data["weather"][0]["icon"]}.png",
+                      width: 150,
+                      height: 150,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ],
+              ),
+              Obx(
+                () => Text(
+                  "${_controller.data["weather"][0]["description"]}",
+                  style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(226, 255, 255, 255)),
                 ),
               ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.water_drop, color: Colors.white),
+                  Obx(
+                    () => Text(
+                      "Humidity: ${_controller.data["main"]["humidity"]}%",
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(226, 255, 255, 255)),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Icon(
+                    Icons.air,
+                    color: Colors.white,
+                  ),
+                  Obx(
+                    () => Text(
+                      "Wind: ${_controller.data["wind"]["speed"]}Km/h",
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(226, 255, 255, 255)),
+                    ),
+                  ),
+                ],
+              )
             ],
           ),
         ),
